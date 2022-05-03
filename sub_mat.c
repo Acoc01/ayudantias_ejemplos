50963599cf4a41d9ad010c0edef3327a088c2a12
Acoc01 <dkaacoc@gmail.com>
2022-05-03 21:26:16 +0000

    corregidos

--- sub_mat.c
+++ sub_mat.c
@@ -1,44 +1,53 @@
 #include <stdio.h>
 
-int main(){
-    int f1,c1,f2,c2;
-    do{
-        scanf("%d %d %d %d", &f1, &c1, &f2, &c2);
-    }while(f1 <= 0 || f2 <= 0 || c1 <= 0 || c2 <= 0 || f1 > f2 || c1 > c2);
-    int mat1[f1][c1];
-    int mat2[f2][c2];
-    for(int i = 0; i < f1; ++i){
-        for(int j = 0; j < c1; ++j){
-            scanf("%d",&mat1[i][j]);
-        }
-    }
+int main (int argc, char *argv[])
+{
+  int n1,n2,m1,m2;
+  do {
+    scanf("%d %d %d %d",&n1, &m1, &n2, &m2);
+  } while(n1 > n2 || m1 > m2 || n1 < 0 || m1 < 0 || n2 < 0 || m2 < 0);
+  
+  int mat1[n1][m1], mat2[n2][m2];
 
-    for(int i = 0; i < f2; ++i){
-        for(int j = 0; j < c2; ++j){
-            scanf("%d",&mat2[i][j]);
-        }
-    }
+  for (int i = 0; i < n1; i++) {
+   for (int j = 0; j < m1; j++) {
+     scanf("%d", &mat1[i][j]);
+     
+   } 
+  }
 
-    int es_submatriz = 0;
-    for(int i = 0; i < (f2-f1)+1; ++i){
-        for(int j = 0; j < (c2-c1)+1; ++j){
-            es_submatriz = 1; //Asumimos que es verdadero
-            for(int k = 0; k < f1; ++k){
-                for(int l = 0; l < c1; ++l){
-                    if(mat1[k][l] != mat2[i+k][j+l]){
-                        es_submatriz = 0;
-                        break;
-                    }
-                }
-                if(!es_submatriz) break;
-            }
-            if(es_submatriz)break;
-        }if(es_submatriz)break;
-    }
-    if(es_submatriz){
-        printf("matriz1 es submatriz de matriz2\n");
-    }else{
-        printf("matriz1 no es submatriz de matriz2\n");
-    }
-    return 0;
+  for (int i = 0; i < n2; i++) {
+   for (int j = 0; j < m2; j++) {
+     scanf("%d", &mat2[i][j]);
+     
+   } 
+  }
+
+  int es_submat = 0;
+
+  for (int i = 0; i <= (n2-n1); i++) {
+    for(int j = 0; j <= (m2-m1); j++){
+      es_submat = 1; //(i,j)
+
+      for (int k = 0; k < n1; k++) {
+       for (int l = 0; l < m1; l++) {
+        if(mat1[k][l] != mat2[i+k][j+l]){
+          es_submat = 0;
+          break;
+        }
+       }if(!es_submat)break; 
+      }
+      if(es_submat)break;
+    }   
+    if(es_submat)break; 
+  }
+
+  if(es_submat){
+   printf("Matriz 1 es sub matriz de Matriz 2\n"); 
+  }else{
+    printf("Matriz 1 no es sub matriz de Matriz 2\n");
+  }
+
+
+  return 0;
 }
